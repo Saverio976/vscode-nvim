58a093a9dd410297ad1b8c261927bd2528342832
Xavier Mitault <xavier.mitault@smobe.fr>
2022-11-29 14:25:48 +0100

    Add empty file for todo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,12 @@ local username = "nvimuser"
 local file_to_load = {
     username .. "." .. "packer-plugins",
     username .. "." .. "theme",
+    username .. "." .. "autocommands",
+    username .. "." .. "commands",
+    username .. "." .. "terminal",
+    username .. "." .. "lsp",
+    username .. "." .. "codeaction",
+    username .. "." .. "options",
 }
 
 for _, file in ipairs(file_to_load) do
