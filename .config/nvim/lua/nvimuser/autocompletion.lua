d44a16829351eb66172990d42fd2760bb046c542
Xavier Mitault <xavier.mitault@smobe.fr>
2022-11-30 15:11:13 +0100

    Add higligth and icons

--- .config/nvim/lua/nvimuser/autocompletion.lua
+++ .config/nvim/lua/nvimuser/autocompletion.lua
@@ -111,11 +111,20 @@ end
 local okwilder, wilder = pcall(require, 'wilder')
 if okwilder then
     wilder.setup({modes = {':', '/'}})
+    wilder.set_option('pipeline', {
+        wilder.branch(
+            wilder.cmdline_pipeline(),
+            wilder.search_pipeline()
+        ),
+    })
     wilder.set_option('renderer', wilder.popupmenu_renderer(
         wilder.popupmenu_border_theme({
             highlights = {
                 border = 'Normal',
             },
+            highlighter = wilder.basic_highlighter(),
+            left = {' ', wilder.popupmenu_devicons()},
+            right = {' ', wilder.popupmenu_scrollbar()},
             border = 'rounded',
         })
     ))
